44fa88535f8cf545e90efc9f78542771d944e4b6
diego-ospina <diego.ospina@evertecinc.com>
2022-08-31 12:15:38 -0500

    feat: created get route to redirect to form index route when a client makes a get request to localhost

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FormController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('form.index');
 });
+
+Route::resource('form', FormController::class);
