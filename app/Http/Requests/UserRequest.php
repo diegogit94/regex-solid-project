4e5ecd151ed97e1c364ac8e244afa9dcea27396c
diego-ospina <diego.ospina@evertecinc.com>
2022-08-31 14:57:46 -0500

    refactor: changed document-type for document_type name in UserRequest to allow massive assignation in one single line using create method of user model

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -19,7 +19,7 @@ class UserRequest extends FormRequest
         return [
             'names' => ['required', 'regex:/^([A-Za-z]([ ]?[A-Za-z]){1,50})$/'],
             'surnames' => ['required', 'regex:/^([A-Za-z]([ ]?[A-Za-z]){1,50})$/'],
-            'document-type' => ['required', 'regex:/^([A-Z]{1,5})$/'],
+            'document_type' => ['required', 'regex:/^([A-Z]{1,5})$/'],
             'identification' => ['required', 'regex:/^([0-9](-?)){1,25}$/'],
             'email' => ['required', 'regex:/^[\w\.]{1,40}@([A-Za-z]{1,10}\.)[A-Za-z]{2,3}$/'],
             'password' => ['required', 'regex:/^.[^ ]{5,50}$/'],
