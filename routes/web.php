4b834529eebb0eb3b286baa40e235ef72ae9da8d
diego-ospina <diego.ospina@evertecinc.com>
2022-08-31 14:52:16 -0500

    refactor: changed default base route to redirect to create form

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\FormController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect()->route('form.index');
+    return redirect()->route('user.create');
 });
 
-Route::resource('form', FormController::class);
+Route::resource('user', UserController::class);
