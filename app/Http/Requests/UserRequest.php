bf4ab857f8678ccdcdf0adc41a19b03f7ee06157
diego-ospina <diego.ospina@evertecinc.com>
2022-08-31 12:13:39 -0500

    fix: changed bad attributes naming and corrected email REGEX

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -8,7 +8,7 @@ class UserRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -17,11 +17,11 @@ class UserRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'regex:/^([A-Za-z]([ ]?[A-Za-z]){1,50})$/'],
+            'names' => ['required', 'regex:/^([A-Za-z]([ ]?[A-Za-z]){1,50})$/'],
             'surnames' => ['required', 'regex:/^([A-Za-z]([ ]?[A-Za-z]){1,50})$/'],
-            'document_type' => ['required', 'regex:/^([A-Z]{1,5})$/'],
+            'document-type' => ['required', 'regex:/^([A-Z]{1,5})$/'],
             'identification' => ['required', 'regex:/^([0-9](-?)){1,25}$/'],
-            'email' => ['required', 'regex:/^[\w-\.]{1,40}@([A-Za-z]{1,10}\.)[A-Za-z]{2,3}$/'],
+            'email' => ['required', 'regex:/^[\w\.]{1,40}@([A-Za-z]{1,10}\.)[A-Za-z]{2,3}$/'],
             'password' => ['required', 'regex:/^.[^ ]{5,50}$/'],
         ];
     }
